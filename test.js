9adc7da212b92356a9c929d356763dc19fb2ea2d
Alex Close <ajclose@gmail.com>
2017-06-12 14:31:16 -0500

    Add viewport tag and add features

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 let calculation = ''
+let operation = []
+let answer = false
 const buttons = document.querySelectorAll('.button')
 const display = document.querySelector('.display')
 let displayContent = display.querySelector('h1')
@@ -9,11 +11,18 @@ function checkPreviousOperation(operator) {
     calculation = calculation.split('')
     calculation[calculation.length-1] = operator
     calculation = calculation.join('')
+  } else if (operator === "√") {
+    calculation += "Math.sqrt("
   } else {
     calculation += operator
     console.log(calculation);
   }
-  }
+}
+
+function evaluate(calculation) {
+  calculation = calculation.join('')
+  return eval(calculation)
+}
 
 
 for (var i = 0; i < buttons.length; i++) {
@@ -21,54 +30,61 @@ for (var i = 0; i < buttons.length; i++) {
   const content = button.querySelector('h1')
   button.addEventListener('click', function(event) {
     if(button.id === "clear") {
-      // displayContent.textContent = 0
       calculation = ''
-      displayContent.textContent = calculation
+      displayContent.textContent = 0
       console.log("cleared!");
     } else if(button.id === "equal") {
+      answer = true
+      console.log(answer);
       console.log("calculating...");
-      if ((eval(calculation))%1 === 0) {
-        calculation = eval(calculation)
-        displayContent.textContent = calculation
-      } else {
-        if((eval(calculation)%1).toString().length>4) {
+
+      if((eval(calculation)%1).toString().length>4) {
         calculation = (eval(calculation)).toFixed(4)
         displayContent.textContent = calculation
     } else {
       calculation = eval(calculation)
       displayContent.textContent = calculation
-      console.log(calculation);
     }
-    }
+
     } else if(button.id ===  "add") {
       checkPreviousOperation("+")
       displayContent.textContent = calculation
+      answer = false
     } else if(button.id ===  "minus") {
       checkPreviousOperation("-")
       displayContent.textContent = calculation
+      answer = false
     } else if(button.id ===  "multiply") {
       checkPreviousOperation("*")
       displayContent.textContent = calculation
+      answer = false
     } else if(button.id ===  "divide") {
       checkPreviousOperation("/")
       displayContent.textContent = calculation
+      answer = false
     } else if(button.id ===  "modulo") {
       checkPreviousOperation("%")
       displayContent.textContent = calculation
-    // } else if(button.id ===  "square-root") {
-    //   if (calculation) {
-    //     calculation = eval(calculation)
-    //     calculation = Math.sqrt(calculation)
-    //     displayContent.textContent = calculation
-    //   } else {
-    //     displayContent.textContent = "√"
-    //   calculation =
-    //   calculation = Math.sqrt(calculation)
-    //   displayContent.textContent = calculation
+      answer = false
+    } else if (button.id === "square-root") {
+      checkPreviousOperation("√")
+      displayContent.textContent = calculation
+      answer = false
+
     } else {
       console.log(content.textContent);
-      calculation += content.textContent
-      displayContent.textContent = calculation
+      if (answer) {
+        calculation = content.textContent
+        displayContent.textContent = calculation
+        answer = false
+        console.log(answer);
+      } else {
+        calculation += content.textContent
+        displayContent.textContent = calculation
+        console.log(answer);
+        answer = false
+      }
+
     }
   })
 }
